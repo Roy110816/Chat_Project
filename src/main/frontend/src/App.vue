5bb08fc565b02fe93af58dcf8a93e01cb2f4b3eb
Roy110816 <3482621067@qq.com>
2025-03-13 19:28:21 +0800

    Initial commit

---
+++ src/main/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <HelloWorld msg="Welcome to Vue in Spring Boot!" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
